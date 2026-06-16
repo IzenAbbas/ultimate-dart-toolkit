fb4f0dde72fff4a2538b0555314abfbcb45a170f
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-06-16 18:46:09 +0500

    feat(abstraction): Abstract Methods

---
+++ dart_oop_practice/04_abstraction/02_abstract_methods.dart
@@ -0,0 +1,31 @@
+abstract class Shape{
+  void shapeInfo(){
+    print('My shape is not specified yet. It totally depends on the child class.');
+  }
+}
+
+class Circle extends Shape{
+  @override
+  void shapeInfo() {
+    // TODO: implement shapeInfo
+    super.shapeInfo();
+    print("Now, my shape is specified as Circle.");
+  }
+}
+
+class Square extends Shape{
+  @override
+  void shapeInfo() {
+    // TODO: implement shapeInfo
+    super.shapeInfo();
+    print("Now, my shape is specified as Square.");
+  }
+}
+
+void main(){
+  Circle c =Circle();
+  Square s = Square();
+  c.shapeInfo();
+  s.shapeInfo();
+}
+
