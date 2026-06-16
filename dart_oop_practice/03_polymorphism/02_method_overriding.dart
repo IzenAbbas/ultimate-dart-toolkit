377e75c1b63cf6b0f546f76370b3273bcd742a42
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-06-16 18:17:56 +0500

    feat(polymorphis): method overriding.

---
+++ dart_oop_practice/03_polymorphism/02_method_overriding.dart
@@ -0,0 +1,30 @@
+class Animal{
+  void sound(){
+    print("Animal is making Sound.");
+  }
+}
+
+class Dog extends Animal{
+  @override
+  void sound() {
+    // TODO: implement sound
+    print("Dog is Barking.");
+  }
+}
+
+class Cat extends Animal{
+  @override
+  void sound() {
+    // TODO: implement sound
+    print("Cat is meowing.");
+  }
+}
+
+void main(){
+  Animal a = Animal();
+  a.sound();
+  Dog d = Dog();
+  a.sound();
+  a = Cat();
+  a.sound();
+}
