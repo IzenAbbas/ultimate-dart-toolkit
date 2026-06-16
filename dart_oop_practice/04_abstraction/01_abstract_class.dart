f6c51cc5bb3f7f32512b358fb1831098914bb14d
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-06-16 18:40:06 +0500

    feat(Abstraction): Abstract Class Basics

---
+++ dart_oop_practice/04_abstraction/01_abstract_class.dart
@@ -0,0 +1,10 @@
+abstract class Shape{
+  void shapeInfo(){
+    print('My shape is not specified yet. It totally depends on the child class');
+  }
+}
+
+void main(){
+  //Shape s = Shape();  It will give error.
+}
+
