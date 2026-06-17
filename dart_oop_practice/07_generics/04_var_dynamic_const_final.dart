20f61f7bbd274a9e2ac2930f7b91a74d554ae27a
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-06-17 11:39:14 +0500

    feat(Gnerics): final, const, var, and dynamic.

---
+++ dart_oop_practice/07_generics/04_var_dynamic_const_final.dart
@@ -0,0 +1,10 @@
+/*
+
+var => Data Type is determined automatically on compile time, once its is determined it cannot be changed.
+
+dynamic => Any datatype is allowed even on run time, It can be changed afterwards.
+
+final => Assigned only once on run time.
+
+const => Assigned only once on compile time.
+ */
