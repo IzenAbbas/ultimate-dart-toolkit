eef68d5243402a85254382bd29bd8d8550ea983c
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-06-17 10:45:45 +0500

    final list

---
+++ dart_oop_practice/06_collections/02_list_final.dart
@@ -0,0 +1,14 @@
+void main(){
+  final List<String> names = [];
+
+  names.add("Izen");
+  names.add("Ali");
+
+  print(names);
+
+  names.add("Ahmed");
+
+  print(names);
+
+  // names = ['A', 'B']; This is not allowed.
+}
