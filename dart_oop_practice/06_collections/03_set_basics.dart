7c41988523dd2a4e10c683ac6386b98d2a55095f
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-06-17 10:49:53 +0500

    feat(Collections): Set Basics.

---
+++ dart_oop_practice/06_collections/03_set_basics.dart
@@ -0,0 +1,17 @@
+void main(){
+  Set<int> numbers = {1,2,3};
+  print(numbers);
+
+  numbers = {1,2,2,2,3};
+  print(numbers);
+
+  numbers.add(6);
+  numbers.add(5);
+  numbers.add(4);
+
+  print(numbers);
+
+  //Union, Intersection, Subtract can be performed via sets
+
+
+}
