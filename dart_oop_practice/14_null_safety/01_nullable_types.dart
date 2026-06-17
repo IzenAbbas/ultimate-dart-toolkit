435fd1869cfec57c8520907d3b2dfdb7522b111e
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-06-17 13:43:16 +0500

    feat(Null Safety): nullable types.

---
+++ dart_oop_practice/14_null_safety/01_nullable_types.dart
@@ -0,0 +1,14 @@
+void main(){
+  String name1= "Izen";
+  print(name1);
+  // But Sting name1 = null will give error.
+
+  String? name2;
+  print(name2);
+
+  String? name3="Ali";
+  print(name3);
+  name3=null;
+  print(name3);
+
+}
